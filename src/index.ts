0f6ddf5f4ad672f0daf3ca258e9eb2cb2d7d26c8
Nick <nmclean@cycle.io>
2022-02-08 22:24:43 -0600

    v0.2.0 - beta

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'module-alias/register';
 import 'source-map-support/register';
 export * from './generate-mongo';
-export * from './mfg-types';
+export * from './types';
 export * from './find-with-pagination';
 export * from './graphql';
