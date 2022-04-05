62b285b52df5b4a51885e2b493030bc224bb2a65
Nick <nmclean@cycle.io>
2022-04-05 08:49:38 -0500

    v0.3.7 - Refactor Nested Parsing

--- src/generate-mongo/parse/parse.ts
+++ src/generate-mongo/parse/parse.ts
@@ -1,29 +1,69 @@
-import { Validate } from '../validate';
-import { FieldFilter } from '../../types';
+import { Validate } from "../validate";
+import { FieldFilter } from "../../types";
 
-export const parseFieldFilters = (object: unknown, rootLocation: string) => {
-  const fieldFilters: { fieldFilter: FieldFilter; location: string }[] = [];
+export const parseFieldFilters = (
+  object: unknown,
+  rootLocation: string
+): {
+  fieldFilter: FieldFilter;
+  location: string;
+}[] => {
+  let fieldFilters: { fieldFilter: FieldFilter; location: string }[] = [];
+
   if (Validate.isValidFieldFilter(object)) {
     fieldFilters.push({ fieldFilter: object, location: rootLocation });
+    return fieldFilters;
   }
 
-  for (const f in object as Record<string, unknown>) {
-    const parsed = parseFieldFilter((object as Record<string, unknown>)[f], [
-      rootLocation,
-      f,
-    ]);
+  if (Array.isArray(object)) {
+    for (const ff in object) {
+      const parsedArrayFilters = parseFieldFilters(object[ff], rootLocation);
+      fieldFilters = [...fieldFilters, ...parsedArrayFilters];
+    }
+    return fieldFilters;
+  }
 
-    if (parsed && !!parsed.fieldFilter) {
-      fieldFilters.push(
-        parsed as { fieldFilter: FieldFilter; location: string }
-      );
-    } else if (!parsed && typeof object === 'object') {
-      const nestedParsed = parseFieldFilters(
-        object,
-        [rootLocation, f].join('.')
-      );
-      for (const nestedFilter of nestedParsed) {
-        fieldFilters.push(nestedFilter);
+  if (typeof object === "object" && object !== null && !Array.isArray(object)) {
+    for (const property in object) {
+      const objectProperty = (object as any)[property];
+
+      if (
+        typeof objectProperty === "object" &&
+        objectProperty !== null &&
+        Array.isArray(objectProperty)
+      ) {
+        for (const i of objectProperty) {
+          const parsedArrayFilters = parseFieldFilters(
+            i,
+            [rootLocation, property].join(".")
+          );
+
+          fieldFilters = [...fieldFilters, ...parsedArrayFilters];
+          continue;
+        }
+      } else if (
+        typeof objectProperty === "object" &&
+        objectProperty !== null &&
+        !Array.isArray(objectProperty)
+      ) {
+        const parsed = parseFieldFilter(objectProperty, [
+          rootLocation,
+          property,
+        ]);
+
+        if (parsed && !!parsed.fieldFilter) {
+          fieldFilters.push(
+            parsed as { fieldFilter: FieldFilter; location: string }
+          );
+        } else if (!parsed && typeof object === "object") {
+          const nestedParsed = parseFieldFilters(
+            object,
+            [rootLocation, property].join(".")
+          );
+          for (const nestedFilter of nestedParsed) {
+            fieldFilters.push(nestedFilter);
+          }
+        }
       }
     }
   }
@@ -34,10 +74,12 @@ export const parseFieldFilters = (object: unknown, rootLocation: string) => {
 export const parseFieldFilter = (
   object: unknown,
   location: string[]
-): {
-  fieldFilter: FieldFilter | undefined;
-  location: string;
-} => {
+):
+  | {
+      fieldFilter: FieldFilter | undefined;
+      location: string;
+    }
+  | undefined => {
   const deepFilterSearch = (object: unknown): FieldFilter | undefined => {
     // If Valid Field Filter, Return, no need to update location.
     if (Validate.isValidFieldFilter(object)) {
@@ -48,7 +90,7 @@ export const parseFieldFilter = (
     for (const k in object as Record<string, unknown>) {
       location.push(k);
       const obj = (object as Record<string, unknown>)[k];
-      if (typeof obj === 'object') {
+      if (typeof obj === "object") {
         return deepFilterSearch(obj as Record<string, unknown>);
       }
     }
@@ -56,9 +98,10 @@ export const parseFieldFilter = (
   };
 
   let fieldFilter: FieldFilter | undefined;
-  if (typeof object === 'object') {
+
+  if (typeof object === "object") {
     fieldFilter = deepFilterSearch(object);
   }
 
-  return { fieldFilter, location: location.join('.') };
+  return { fieldFilter, location: location.join(".") };
 };
